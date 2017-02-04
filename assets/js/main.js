9e72caa88e5d97207690d328fdd44eb56f380dd4
MinimalistYing <kingying94@gmail.com>
2017-02-04 18:00:57 +0800

    add to-top div

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,12 @@ $(function(){
             scrollTop: 0
         }, 1000);
 	})
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 50) {
+			$('#to-top').removeClass('hide');
+		} else {
+			$('#to-top').addClass('hide');
+		}
+	})
 })
