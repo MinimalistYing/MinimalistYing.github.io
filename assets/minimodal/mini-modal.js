4dc9d542b509a236c59fb9cf43f60c7da07cb431
MinimalistYing <kingying94@gmail.com>
2017-02-20 19:09:07 +0800

    change minimodal.js

--- assets/minimodal/mini-modal.js
+++ assets/minimodal/mini-modal.js
@@ -1,30 +1,43 @@
 (function($, window){
-	var MiniModal = function (element, options) {
-        var opt = $.extend({}, this.DEFAULTS, options);
-		this.init(element, opt);
-	};
-
-	MiniModal.prototype.VERSION = 1.0;
-
-    MiniModal.prototype.DEFAULTS = {
-    	width : 500,
-    	height : 300,
-    	top: 200,
-    	backgroundColor: 'white',
-        bodyContent : '添加一点内容吧~',
-        hasFoot : true
+    var DEFAULTS = {
+        width : 500,
+        height : 300,
+        top: 200,
+        backgroundColor: 'white',
+        bodyContent : 'Add something hear~',
+        bodyTextAlign: 'center',
+        footContent : 'Add something hear~',
+        footTextAlign: 'center',
+        hasFoot : true,
+        defaultShow : false,
+        onDestroy: function(){}
     }
-
-    MiniModal.prototype.TEMPLATES = {
+    
+    var TEMPLATES = {
             wrapper : '<div class="minimodal-wrapper"></div>',
             body    : '<div class="minimodal-body"></div>',
             foot    : '<div class="minimodal-foot"></div>',
             shade   : '<div class="minimodal-shade"></div>', 
     }
 
-    MiniModal.prototype.init = function(element, options) {
-        var renderModal = $.proxy(function () {
-            var $shade = $(this.TEMPLATES.shade)
+    var MiniModal = function (element, options) {
+        var opt = $.extend({}, DEFAULTS, options);
+        init(element, opt);
+    };
+
+    MiniModal.prototype.VERSION = 1.0;
+
+    var init = function(element, options) {  
+        var $wrapper = renderModal(options);
+        setStyle($wrapper, options);
+
+        if (!options.defaultShow) {//hide modal
+            $wrapper.hide();
+        }
+    }
+
+    var renderModal = function (options) {
+            var $shade = $(TEMPLATES.shade)
                          .appendTo($('body'))
                          .click(function(e){
                             var top    = parseFloat($shade.find('.minimodal-wrapper').css('top')),
@@ -33,37 +46,44 @@
                                 right  = window.innerWidth - left;
                             if (e.clientX < left || e.clientX > right || e.clientY < top || e.clientY > bottom) {
                                 $shade.remove();
+                                if (typeof options.onDestroy === 'function') {
+                                    options.onDestroy();
+                                }
                             }
                             
                          });
-            var $wrapper = $(this.TEMPLATES.wrapper).append($(this.TEMPLATES.body)).appendTo($shade);
+            var $wrapper = $(TEMPLATES.wrapper)
+                               .append($(TEMPLATES.body))
+                               .appendTo($shade);
             if (options.hasFoot === true) {
-                $wrapper.append($(this.TEMPLATES.foot));
+                $wrapper.append($(TEMPLATES.foot)
+                        .append(options.footContent));
             }
             return $wrapper;
-        },this);  
-    	var $wrapper = renderModal();
-    	$wrapper.css({
-    		width  : options.width,
-    		height : options.height,
-    		top    : options.top,
-    		backgroundColor : options.backgroundColor,
-    		left   : (window.innerWidth - options.width)/2
-    	});
+    };
+
+    var setStyle = function ($wrapper, options) {
+        $wrapper.css({
+            width  : options.width,
+            height : options.height,
+            top    : options.top,
+            backgroundColor : options.backgroundColor,
+            left   : (window.innerWidth - options.width)/2
+        });
         $wrapper.find('.minimodal-body').css({
-            height : options.height * 7/8
+            height : options.height * 7/8,
+            textAlign : options.bodyTextAlign
         }).append(options.bodyContent);
         $wrapper.find('.minimodal-foot').css({
-            height : options.height * 1/8
-        }).append(options.footContent || '');
-    	this.$element = element;
-
+            height : options.height * 1/8,
+            textAlign : options.footTextAlign
+        });
     }
 
-	$.fn.minimodal = function (options) {
-		return this.each(function(){
-			var modal = new MiniModal($(this), options);
+    $.fn.minimodal = function (options) {
+        return this.each(function(){
+            var modal = new MiniModal($(this), options);
         })
-	}
+    }
 
 })(jQuery, window);
