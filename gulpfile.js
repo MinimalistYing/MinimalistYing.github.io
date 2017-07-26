ecdc42845f48bf780e0d0ba9be443e366ab1bedf
MinimalistYing <kingying94@gmail.com>
2017-07-26 21:00:45 +0800

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp= require('gulp'),
-	  less= require('gulp-less');
+	  less= require('gulp-less')
 
 gulp.task('less', () => {
 	return gulp.src('src/less/**/*.less')
 		.pipe(less())
-		.pipe(gulp.dest('assets/css'));
+		.pipe(gulp.dest('assets/css'))
 })
 
-gulp.task('default', ['less']);
+gulp.task('less:watch', () => {
+	gulp.watch('src/less/**/*.less', ['less'])
+})
+
+gulp.task('default', ['less', 'less:watch'])
