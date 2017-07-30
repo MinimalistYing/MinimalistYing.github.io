8d73e08ba48f85b668cbd03ee965836468404a74
MinimalistYing <kingying94@gmail.com>
2017-07-30 18:26:49 +0800

    add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp= require('gulp'),
 	  less= require('gulp-less')
 	  plumber = require('gulp-plumber')
+	  autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('less', () => {
 	return gulp.src('src/less/**/*.less')
 		.pipe(plumber())
 		.pipe(less())
+		.pipe(autoprefixer({browsers: ['last 4 versions']}))
 		.pipe(gulp.dest('assets/css'))
 })
 
