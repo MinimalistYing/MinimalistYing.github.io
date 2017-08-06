1c30b7b27dd8d3c96e84021f72bb98006d0fd88e
MinimalistYing <kingying94@gmail.com>
2017-08-06 19:01:15 +0800

    change header hover state

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+	var $nav = document.getElementsByTagName('nav')[0]
+	var $a  = $nav.getElementsByTagName('a')
+
+	for (let i = $a.length - 1; i >= 0; i--) {
+		let $aChild = $a[i];
+
+		$a[i].parentNode.onclick = function() {
+			$aChild.click();
+		}
+	}
+}
