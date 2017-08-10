9c176af4bd18a32ce2b7b0315fe66c79d872e6d6
MinimalistYing <kingying94@gmail.com>
2017-08-10 19:52:06 +0800

    add memo-card hover state

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,19 @@ window.onload = function() {
 	for (let i = $a.length - 1; i >= 0; i--) {
 		let $aChild = $a[i];
 
-		$a[i].parentNode.onclick = function() {
+		$a[i].parentNode.onclick = () => {
 			$aChild.click();
+		}
+	}
+
+	var $cards = document.getElementsByClassName('memo-card')
+
+	for (let i = 0; i < $cards.length; i++) {
+		$cards[i].onmouseenter = () => {
+			console.info($cards[i].getElementsByClassName('memo-time')[0])
+			let e = document.createEvent("MouseEvents");
+			e.initEvent("onmouseenter", true, true);
+			$cards[i].getElementsByClassName('memo-time')[0].dispatchEvent(e);
 		}
 	}
 }
