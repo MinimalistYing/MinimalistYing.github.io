65ed49959fc57f2c8b2a7f853f6ef9ada802de58
MinimalistYing <kingying94@gmail.com>
2017-08-19 18:27:15 +0800

    add comment

--- gulp-simple-markdown.js
+++ gulp-simple-markdown.js
@@ -20,26 +20,40 @@ function markdown() {
 	    // buffer对象可以操作
         if (file.isBuffer()) {
             //拿到单个文件buffer             
-            file.contents = new Buffer(file.contents.toString("utf-8"),"utf-8");
+            file.contents = new Buffer(file.contents.toString("utf-8"),"utf-8")
         }
          
         // stream流是不能操作的,可以通过fs.readFileSync
         if (file.isStream()) {
             // 同步读取
-            file.contents = new Buffer(s.readFileSync(file.path).toString("utf-8"),"utf-8");
+            file.contents = new Buffer(s.readFileSync(file.path).toString("utf-8"),"utf-8")
         }
 
-        originHtml = file.contents.toString("utf-8");// 转化为字符串
-        result = originHtml.replace(/ `(.+?)` /g, '<pre>$1</pre>');
-        file.contents = new Buffer(result);
+        originHtml = file.contents.toString("utf-8")// 转化为字符串
+        result = code(originHtml)
+        file.contents = new Buffer(result)
 
 		this.push(file)
+		// 声明文件已处理完毕
 		callback()
 	},function(callback) {
-      callback();
+		// 声明文件已处理完毕
+    	callback()
     });
 
-    return stream;// 最后将文件流返回
+    return stream// 最后将文件流返回
+}
+
+/**
+ * 将Markdown代码转化为HTML
+ * 把`..`替换为<pre></pre> 
+ * 对其中的特殊字符进行转义
+ * 把换行替换为<br>
+ * @param  {[String]} text [原始文本]
+ * @return {[String]}      [转化后的文本]
+ */
+function code(text) {
+	return text.replace(/ `(.+?)` /g, '<pre>$1</pre>')
 }
 
 module.exports = markdown;
