cf9ad46d47a225186b816cd656750009a888c6d8
MinimalistYing <kingying94@gmail.com>
2018-04-29 21:15:51 +0800

    change to React NavLink

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import './style.less'
 
@@ -9,11 +10,15 @@ class Header extends React.Component {
 			    <h3 className="header-title">MinimalistYing.io</h3>
 			    <nav>
 			        <ul>
-			            <li className="selected"><a href="./index.html">Home</a></li>
-			            <li ><a href="./dist/memo.html">Memo</a></li>
-			            <li ><a href="https://jsfiddle.net/user/MinimalistYing/fiddles/">CodeBase</a></li>
-			            <li ><a href="https://www.hackerrank.com/MinimalistYing">HackerRank</a></li>
-			            <li ><a href="https://github.com/MinimalistYing">Github</a></li>
+			            <li className="selected">
+			            	<NavLink to="/">Home</NavLink>
+			            </li>
+			            <li>
+			            	<NavLink to="/memo">Memo</NavLink>
+			           	</li>
+			            <li><a href="https://jsfiddle.net/user/MinimalistYing/fiddles/">CodeBase</a></li>
+			            <li><a href="https://www.hackerrank.com/MinimalistYing">HackerRank</a></li>
+			            <li><a href="https://github.com/MinimalistYing">Github</a></li>
 			        </ul>
 			    </nav>
 			</header>
