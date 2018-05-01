817ab5d019d6cf785c3fa3e92d7e76230017a992
MinimalistYing <kingying94@gmail.com>
2018-05-01 20:45:35 +0800

    fix nav hover highlight

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,11 +10,11 @@ class Header extends React.Component {
 			    <h3 className="header-title">MinimalistYing.io</h3>
 			    <nav>
 			        <ul>
-			            <li className="selected">
-			            	<NavLink to="/">Home</NavLink>
+			            <li>
+			            	<NavLink to="/" exact>Home</NavLink>
 			            </li>
 			            <li>
-			            	<NavLink to="/memo">Memo</NavLink>
+			            	<NavLink to="/memo" exact>Memo</NavLink>
 			           	</li>
 			            <li><a href="https://jsfiddle.net/user/MinimalistYing/fiddles/">CodeBase</a></li>
 			            <li><a href="https://www.hackerrank.com/MinimalistYing">HackerRank</a></li>
