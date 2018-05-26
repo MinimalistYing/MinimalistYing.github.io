3c76faf91f6feaf64ddcda43b5943b732dd10fde
MinimalistYing <kingying94@gmail.com>
2018-05-26 18:33:32 +0800

    react build set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,45 @@
-const gulp= require('gulp'),
-	  less= require('gulp-less'),
-	  plumber = require('gulp-plumber'),
-	  autoprefixer = require('gulp-autoprefixer'),
-	  cleanCSS = require('gulp-clean-css'),
-	  markdown = require('gulp-markdown')
-	  simpleMarkDown = require('./gulp-simple-markdown')
+const gulp= require('gulp')
+// 	  less= require('gulp-less'),
+// 	  plumber = require('gulp-plumber'),
+// 	  autoprefixer = require('gulp-autoprefixer'),
+// 	  cleanCSS = require('gulp-clean-css'),
+// 	  markdown = require('gulp-markdown')
+// 	  simpleMarkDown = require('./gulp-simple-markdown')
 
-// 预处理less
-gulp.task('less', () => {
-	return gulp.src('src/less/**/*.less')
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(autoprefixer({browsers: ['last 4 versions']}))
-		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest('dist/assets/css'))
+// // 预处理less
+// gulp.task('less', () => {
+// 	return gulp.src('src/less/**/*.less')
+// 		.pipe(plumber())
+// 		.pipe(less())
+// 		.pipe(autoprefixer({browsers: ['last 4 versions']}))
+// 		.pipe(cleanCSS({compatibility: 'ie8'}))
+// 		.pipe(gulp.dest('dist/assets/css'))
+// })
+
+// // 预处理自己用部分MarkDown语法写的HTML
+// gulp.task('simplemarkdown', () => {
+// 	return gulp.src('src/view/memo.html')
+// 		.pipe(simpleMarkDown())
+// 		.pipe(gulp.dest('dist/'))
+// })
+
+// // 将博客的markdown转化为html
+// gulp.task('markdown', () => {
+// 	return gulp.src('src/blog/**/*.md')
+// 		.pipe(markdown())
+// 		.pipe(gulp.dest('dist/blog/'))
+// })
+
+// // 监听文件改变 对改变的文件进行预处理
+// gulp.task('watch', () => {
+// 	gulp.watch('src/less/**/*.less', ['less'])
+// 	gulp.watch('src/view/memo.html', ['simplemarkdown'])
+// 	gulp.watch('src/blog/**/*.md', ['markdown'])
+// })
+
+// gulp.task('default', ['less', 'simplemarkdown', 'watch', 'markdown'])
+
+gulp.task('default', () => {
+	return gulp.src('dist/index.html')
+		.pipe(gulp.dest('./'))
 })
-
-// 预处理自己用部分MarkDown语法写的HTML
-gulp.task('simplemarkdown', () => {
-	return gulp.src('src/view/memo.html')
-		.pipe(simpleMarkDown())
-		.pipe(gulp.dest('dist/'))
-})
-
-// 将博客的markdown转化为html
-gulp.task('markdown', () => {
-	return gulp.src('src/blog/**/*.md')
-		.pipe(markdown())
-		.pipe(gulp.dest('dist/blog/'))
-})
-
-// 监听文件改变 对改变的文件进行预处理
-gulp.task('watch', () => {
-	gulp.watch('src/less/**/*.less', ['less'])
-	gulp.watch('src/view/memo.html', ['simplemarkdown'])
-	gulp.watch('src/blog/**/*.md', ['markdown'])
-})
-
-gulp.task('default', ['less', 'simplemarkdown', 'watch', 'markdown'])
---
+++ webpack.build.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: './dist',
+		filename: '[name].[hash].bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /.(jsx|js)$/,
+			exclude: /node_modules/,
+			use: ['babel-loader']
+		}, {
+			test: /.less$/,
+			exclude: /node_modules/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: ['css-loader', 'less-loader']
+	        })
+		}, {
+			test: /.(jpe?g|png|gif|svg)$/,
+			exclude: /node_modules/,
+			use: [{
+				loader: 'url-loader',
+				options: {
+					limit: 8192 // 小于1kb的图片采用base64编码 并以DATAUrl的形式嵌入页面
+				}
+			}]
+		}]
+	},
+	devtool: 'inline-source-map',
+	plugins: [
+		new webpack.NamedModulesPlugin(),
+		new CleanWebpackPlugin(['dist']),
+		new ExtractTextPlugin({
+			filename: 'css/[name].[contenthash].css'
+		}),
+		new HtmlWebpackPlugin({
+			template: './react-index.html',
+			filename: 'index.html'
+		})
+	]
+}
