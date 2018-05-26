8b68983edb39b0deca24a9f54dff372565e9f298
MinimalistYing <kingying94@gmail.com>
2018-05-26 18:58:22 +0800

    change to hash router

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {
-	BrowserRouter as Router,
+	HashRouter as Router,
 	Route,
 	Link
 } from 'react-router-dom'
