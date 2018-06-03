7121f090950c257fd3aba4498a45b877cbd09f10
MinimalistYing <kingying94@gmail.com>
2018-06-03 16:06:47 +0800

    migrate gulp and css memos done

--- src/components/Memos/index.jsx
+++ src/components/Memos/index.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import Memo from '../Memo/index.jsx'
 
 import JavaScript from '../../memos/js.js'
+import Css from '../../memos/css.js'
+import Gulp from '../../memos/gulp.js'
 
-import Memo from '../Memo/index.jsx'
+import './index.less'
 
 class Memos extends React.Component {
 	render() {
 		return (
 			<main className="memos">
-				{
-					JavaScript.map((o, index) => <Memo key={index} data={o} />)
-				}
+				<section className="js memo-wrap">
+					{
+						JavaScript.map((o, index) => <Memo key={index} data={o} />)
+					}
+				</section>
+				
+				<section className="css memo-wrap">
+					{
+						Css.map((o, index) => <Memo key={index} data={o} />)
+					}
+				</section>
+
+				<section className="gulp memo-wrap">
+					{
+						Gulp.map((o, index) => <Memo key={index} data={o} />)
+					}
+				</section>
 			</main>
 		)
 	}
