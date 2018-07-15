5a1b0eb7b9bc7ab6001492fe6f48277bd9725d24
MinimalistYing <kingying94@gmail.com>
2018-07-15 11:12:58 +0800

    add reponsive layout

--- src/components/Memos/index.jsx
+++ src/components/Memos/index.jsx
@@ -16,62 +16,25 @@ const {
 	vue,
 	webpack
 } = memos
+const all = [
+	...compability,
+	...css,
+	...http,
+	...jquery,
+	...js,
+	...other,
+	...react,
+	...vue,
+	...webpack
+]
 
 class Memos extends React.Component {
 	render() {
 		return (
 			<main className="memos">
-				<section className="js memo-wrap">
+				<section className="memo-wrap">
 					{
-						js.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-				
-				<section className="css memo-wrap">
-					{
-						css.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="vue memo-wrap">
-					{
-						vue.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="react memo-wrap">
-					{
-						react.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="webpack memo-wrap">
-					{
-						webpack.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="jquery memo-wrap">
-					{
-						jquery.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="http memo-wrap">
-					{
-						http.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="compability memo-wrap">
-					{
-						compability.map((o, index) => <Memo key={index} data={o} />)
-					}
-				</section>
-
-				<section className="other memo-wrap">
-					{
-						other.map((o, index) => <Memo key={index} data={o} />)
+						all.map((o, index) => <Memo key={index} data={o} />)
 					}
 				</section>
 			</main>
