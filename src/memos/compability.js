72db23119c59395acfebdf54b69ad77bb3ae2460
影子 <kingying94@gmail.com>
2018-08-20 10:52:49 +0800

    fix error about e.path

--- src/memos/compability.js
+++ src/memos/compability.js
@@ -152,7 +152,26 @@ window.oncontextmenu = e => e.preventDefault()
 \`\`\`js
 // e.path Chrome采用 非标准属性
 // e.composedPath() 标准属性 最新的 FF Chrome Safari都兼容
-const path = e.path || e.composedPath()
+const path = e.path || (e.composedPath && e.composedPath())
+
+// 如果需要兼容更低版本的浏览器 可以自己去遍历
+function getPath(e) {
+	const path = []
+	let dom = e.target || e.srcElement
+	while (dom) {
+		path.push(dom)
+		
+		// 为了和composedPath()行为一致
+		if (dom.tagName === 'HTML') {
+            path.push(document)
+            path.push(window)
+
+            return path
+       }
+
+		dom = dom.parentNode
+	}
+}
 \`\`\`
 去获取这个事件从触发事件的Dom节点开始到Window的Dom路径
 `
