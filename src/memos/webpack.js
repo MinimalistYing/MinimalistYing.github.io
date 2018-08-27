d547cdfa8511d24134ca2c93bd1274dd19d1d282
影子 <kingying94@gmail.com>
2018-08-27 10:47:22 +0800

    about webpack alias with eslint

--- src/memos/webpack.js
+++ src/memos/webpack.js
@@ -58,4 +58,24 @@ plugins: [
 ]
 \`\`\`
 `
+}, {
+	date: `2018/8/27`,
+	content: `
+在使用webpack提供的alias特性时，如果配置了eslint的import/no-unresolved规则  
+会发现eslint并不会识别alias，然后当你使用alias时会报错  
+这时需要借助[eslint-import-resolver-webpack](https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers/webpack)  
+然后再.eslintrc文件中增加配置项  
+\`\`\`js
+{
+	settings: {
+		'import/resolver': {
+			webpack: {
+				// 配置alias的文件路径
+				config: './webpack.base.js'
+			}
+		}
+	}
+}
+\`\`\`
+`
 }]
