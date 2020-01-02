7026c24d4a3958f3733328f4d8cd5e643e7e805a
MinimalistYing <genius_ying@163.com>
2020-01-02 11:10:56 +0800

    支持 https

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
+
 const app = express()
-var https = require('https')
-var http = require('http')
 
 app.use(express.static('./dist'))
 
 http.createServer(app).listen(80)
-https.createServer({}, app).listen(443)
+
+https.createServer({
+  key: fs.readFileSync('./3294475_minimalistying.com_other/3294475_minimalistying.com.key'),
+  cert: fs.readFileSync('./3294475_minimalistying.com_other/3294475_minimalistying.com.pem')
+}, app).listen(443)
