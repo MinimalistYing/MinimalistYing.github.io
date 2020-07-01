9364f365004346a985465d5f2366f24eff6482cc
MinimalistYing <genius_ying@163.com>
2020-07-01 16:15:37 +0800

    服务器增加代理服务

--- server.js
+++ server.js
@@ -7,6 +7,32 @@ const app = express()
 
 app.use(express.static('./dist'))
 
+/**
+ * 代理请求，获取 00 后黑话翻译
+ */
+app.get('/guess-proxy', function (req, res) {
+  const proxyReq = https.request({
+    hostname: 'lab.magiconch.com',
+    port: 443,
+    path: '/api/nbnhhsh/guess',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, (proxyRes) => {
+    proxyRes.setEncoding('utf8')
+    proxyRes.on('data', (d) => {
+      res.status(200).json(d)
+    })
+  })
+  
+  proxyReq.on('error', (e) => {
+    res.status(500).json({ message: '服务器错误', error: e})
+  })
+  proxyReq.write(JSON.stringify({ text: req.query.text }))
+  proxyReq.end()
+})
+
 http.createServer(app).listen(80)
 
 https.createServer({
